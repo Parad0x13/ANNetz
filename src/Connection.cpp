89d30ca3a0df005b641cf5c5a38b51dc73bd2a5b
Bryan Tremblay <bryanandhallie@gmail.com>
2017-05-16 06:58:55 -0400

    Refactored how project is compiled in C++. Instead of having a prebuilt VS solution I've instead opted to use CMake to do it external to source. Things should be MUCH nicer now and allow us to cross compile this project with ease

---
+++ src/Connection.cpp
@@ -0,0 +1,24 @@
+#include "Connection.h"
+#include "Utility.h"
+
+using namespace std;
+
+Connection::Connection(Neuron* _a, Neuron* _b, double _weight) {
+	a = _a;
+	b = _b;
+	weight = _weight;
+}
+
+void Connection::setRandomWeights() {
+	weight = normalDistribution();
+}
+
+ostream& operator<<(ostream& stream, Connection& c) {
+	stream << "Connection " << &c << ": ";
+
+	// [TODO] Add error checking for NULL connections
+
+	stream << c.a << " -> " << c.b << " = " << c.weight;
+
+	return stream;
+};
